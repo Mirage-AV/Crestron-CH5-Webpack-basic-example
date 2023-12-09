64dc0e6c8a6b1febf53b8136cbb4c55bc777341f
jphillipsCrestron <jphillips@crestron.com>
2023-12-08 20:07:05 -0600

    Update CH5 to 2.6.1

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-import * as CrComLib from "@crestron/ch5-crcomlib";
-import * as WebXPanel from "@crestron/ch5-webxpanel";
-import './styles/main.scss'
+import CrComLib from "@crestron/ch5-crcomlib";
+import { getWebXPanel, runsInContainerApp } from "@crestron/ch5-webxpanel";
+import './styles/main.scss';
 
-import { bridgeReceiveIntegerFromNative, bridgeReceiveBooleanFromNative, bridgeReceiveStringFromNative, bridgeReceiveObjectFromNative }
-from "@crestron/ch5-crcomlib";
-window.CrComLib = CrComLib; //need this for xpanel subscribeState to work
-window["bridgeReceiveIntegerFromNative"] = bridgeReceiveIntegerFromNative;
-window["bridgeReceiveBooleanFromNative"] = bridgeReceiveBooleanFromNative;
-window["bridgeReceiveStringFromNative"] = bridgeReceiveStringFromNative;
-window["bridgeReceiveObjectFromNative"] = bridgeReceiveObjectFromNative;
+CrComLib = CrComLib.CrComLib; // Re-assign CrComLib local variable to the CrComLib.CrComLib object that exists in 2.6+
+window.CrComLib = CrComLib; // Assign this CrComLib local variable to the Window object for WebXPanel access
+
+window.bridgeReceiveIntegerFromNative = CrComLib.bridgeReceiveIntegerFromNative;
+window.bridgeReceiveBooleanFromNative = CrComLib.bridgeReceiveBooleanFromNative;
+window.bridgeReceiveStringFromNative = CrComLib.bridgeReceiveStringFromNative;
+window.bridgeReceiveObjectFromNative = CrComLib.bridgeReceiveObjectFromNative;
+
+const { WebXPanel, isActive, WebXPanelConfigParams, WebXPanelEvents, getVersion, getBuildDate } = getWebXPanel(!runsInContainerApp());
 
 //Web Xpanel
 const configuration = {
-    host: "192.168.1.218",
-    ipId: "0x42"
+    host: "192.168.1.223",
+    ipId: "0x04"
 }
 
-if (WebXPanel.isActive) {
+if (isActive) {
 
-    console.log(`WebXPanel version: ${WebXPanel.getVersion()}`);
-    console.log(`WebXPanel build date: ${WebXPanel.getBuildDate()}`);
+    console.log(`WebXPanel version: ${getVersion()}`);
+    console.log(`WebXPanel build date: ${getBuildDate()}`);
 
-    WebXPanel.default.initialize(configuration);
-    WebXPanel.default.addEventListener(WebXPanel.WebXPanelEvents.CONNECT_CIP, ({ detail }) => {
+    WebXPanel.initialize(configuration);
+
+    window.addEventListener(WebXPanelEvents.CONNECT_CIP, ({ detail }) => {
         const { url, ipId, roomId } = detail;
         console.log(`Connected to ${url}, 0x${ ipId.toString(16)}, ${roomId}`);
     });
-    WebXPanel.default.addEventListener(WebXPanel.WebXPanelEvents.DISCONNECT_CIP, ({ detail }) => {
+    window.addEventListener(WebXPanelEvents.DISCONNECT_CIP, ({ detail }) => {
         const { reason } = detail;
         console.log(`Disconnected from CIP. Reason: ${reason}`);
     });
 }
-
-
 
 // Basic Example of a single button
 const button = document.getElementById("hello");
@@ -40,11 +41,11 @@ const button = document.getElementById("hello");
 button.addEventListener("click", function() {
     CrComLib.publishEvent("b", "1", true);
     CrComLib.publishEvent("b", "1", false);
-});  
+}); 
 
 
-CrComLib.subscribeState("b",  "1",   (value)  =>  {         
-    if  (value)  {       button.classList.add("demo--selected");          }        
-    else  {             button.classList.remove("demo--selected");         }            
-});   
-document.getElementById("hello").click();  
+CrComLib.subscribeState("b", "1",  (value) => {
+    if (value) { button.classList.add("demo--selected"); }
+    else { button.classList.remove("demo--selected"); }
+});
+document.getElementById("hello").click();
